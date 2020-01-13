1816f4d8aa84b2d2d243939ddad7432f82e2f3e1
Scott <swschipke@gmail.com>
2020-01-12 20:05:00 -0700

    Add apiCall for getting current photo

---
+++ src/util/apiCalls.js
@@ -0,0 +1,15 @@
+export const getTodaysPhoto = async () => {
+  const url = process.env.VUE_APP_TODAYS_PHOTO_URL;
+  console.log(url)
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('There was an error in data analysis.');
+    }
+    let data = await response.json()
+    return data
+  } catch (error) {
+    console.error('WRONG!!')
+    throw new Error(error);
+  }
+}
