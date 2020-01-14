b59432a8403f31d00d17ea0742238f8dbbe4624a
Scott <swschipke@gmail.com>
2020-01-13 20:40:12 -0700

    Begin rendering month photos conditionally

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <TodaysPhoto v-bind:image="this.currentPhoto"></TodaysPhoto>
+    <NavBar v-bind:monthView="this.monthView" @click="changeMonthView"></NavBar>
+    <TodaysPhoto  v-bind:image="this.currentPhoto" v-if="!this.monthView"></TodaysPhoto>
+    <MonthPhotos v-if="this.monthView"></MonthPhotos>
   </div>
 </template>
 
 <script>
 import TodaysPhoto from './components/TodaysPhoto.vue'
-import {getTodaysPhoto} from './util/apiCalls'
+import NavBar from './components/NavBar.vue'
+import MonthPhotos from './components/MonthPhotos.vue'
+import {getTodaysPhoto, getMonthPhotos} from './util/apiCalls'
+
 
 export default {
   name: 'app',
   components: {
-    TodaysPhoto
+    NavBar,
+    TodaysPhoto,
+    MonthPhotos
   },
   data() {
     return {
-    currentPhoto: null
+    currentPhoto: null,
+    allPhotos: [],
+    monthView: false
     }
   },
+  methods: {
+    changeMonthView(value) {
+    this.monthView = value
+  }
+},
   mounted() {
   console.log('mounted!')
     getTodaysPhoto()
     .then(res => this.currentPhoto = res)
     .catch(err => console.error('Error fetching current photo:', err))
+    getMonthPhotos()
+    .then(res => this.allPhotos = res)
+    .catch(err => console.error('Error fetching all photos:', err))
+
   }
 }
 </script>
@@ -34,6 +52,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MonthPhotos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>Now you see me!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Month View',
+  props: {
+    image: Object
+  },
+  methods: {
+  tester: async e => {
+  e.preventDefault()
+    alert(`It worked! name: ${e.target}!`)
+    }
+  } 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  height: 70vh;
+  width: 70vh;
+  }
+</style>
