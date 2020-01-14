130b541ae74aa7d76428cf629cc1e4c1dc6c6d01
Scott <swschipke@gmail.com>
2020-01-13 20:39:46 -0700

    Add apiCall to fetch all month of photos

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -13,3 +13,19 @@ export const getTodaysPhoto = async () => {
     throw new Error(error);
   }
 }
+
+export const getMonthPhotos = async () => {
+  const url = process.env.VUE_APP_MONTH_PHOTOS_URL;
+  console.log(url)
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('There was an error in data analysis.');
+    }
+    let data = await response.json()
+    return data
+  } catch (error) {
+    console.error('WRONG!!')
+    throw new Error(error);
+  }
+}
