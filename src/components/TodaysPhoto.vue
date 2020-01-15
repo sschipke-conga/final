ca49a49774bbb45d59b3a24efef4134a185cb88d
Scott <swschipke@gmail.com>
2020-01-14 18:52:26 -0700

    Splitscreen photo and information

--- src/components/TodaysPhoto.vue
+++ src/components/TodaysPhoto.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="hello">
+  <div class="current-photo">
+    <div class="div-description">
     <h1>{{ image.title }}</h1>
-    <img v-bind:src="image.hdurl" alt="astronomy">
+      <p>{{image.explanation}}</p>
     <h2>{{image.date}}</h2>
-    <p>{{image.explanation}}</p>
+    </div>
+      <img v-bind:src="image.hdurl" alt="astronomy">
   </div>
 </template>
 
@@ -12,20 +14,36 @@ export default {
   name: 'Current Photo',
   props: {
     image: Object
-  },
-  methods: {
-  tester: async e => {
-  e.preventDefault()
-    alert(`It worked! name: ${e.target}!`)
-    }
-  } 
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.current-photo {
+  display: flex;
+  height: 100%;
+  width: 100vw;
+  }
+.div-description {
+  background: AliceBlue;
+  border: 5px solid AliceBlue;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 10px;
+  height: 80vh;
+  width: 50vw;
+  }
 img {
-  height: 70vh;
-  width: 100vh;
+  height: 80vh;
+  width: 50vw;
+  }
+  p, h1, h2 {
+  background: AliceBlue;
+  }
+  p {
+  padding: 10px;
   }
 </style>
